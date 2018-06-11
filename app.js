42c67e0471ce4253f8d54451232070b9611720e9
Rohan Harrison <harrison.rohan@gmail.com>
2018-06-10 18:26:43 -0700

    Register/Body-Parser Example

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 //Include
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 
 //Config
 app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //Routes, routes.js
 routes = require('./routes')(app);
--- routes.js
+++ routes.js
@@ -1,9 +1,18 @@
 var app = require('./app');
 
 module.exports = function(app){
-
+    //Index
     app.get('/', function (req, res) {
         res.render('index', { title: 'Test', message: 'Passing message from routes' })
+    });
+
+    //Register
+    app.get('/register', function (req, res) {
+        res.render('register')
+    });
+
+    app.post('/register', function (req, res) {
+        res.send(req.body)
     });
 
 }
